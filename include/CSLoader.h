df6dee9e7731ddc3c185edc505e2a57faa9fcefa
Anrokyr <78145373+ShiroNoby@users.noreply.github.com>
2024-10-31 19:40:16 +0500

    its crossplatform now

--- include/CSLoader.h
+++ include/CSLoader.h
@@ -1,8 +1,7 @@
 #ifndef _CS_Loader_
 #define _CS_Loader_
 
-#include <windows.h>
-#include <WinBase.h>
+#include <filesystem>
 
 #include "CSPlaylist.h"
 
@@ -24,68 +23,21 @@ public:
 void Loader::LoadPlaylist( CSPlaylist& Playlist ){
     SystemLog( tier2, "CSLoader.h", "24", "Loader::LoadPlaylist");
     
-    HANDLE                  path;
-    _WIN32_FIND_DATAW       SPath;
-    wchar_t                 resoult[MAX_PATH];   
-    
-    /////////////**************************************\\\\\\\wav
-    
-    SystemLog( tier2, "CSLoader.h", "34", "load wav");
-    path = FindFirstFile( L"../sound/*.wav*", &SPath);
-    do {
-
-        if (path == INVALID_HANDLE_VALUE) { //no treck
-            ErrorLog( tier2, "CSLoader.h", "37", "FindFirstFile failed", 999 ); //unknow error
-            break;
-        }
-
-
-        //func( CSPath[index], resoult);
-        func( L"../sound/", resoult);
-        std::wstring str = resoult;
-
-        func(SPath.cFileName, resoult);
-        str += resoult;
-
-        Playlist.playlist.push_back(str);
-
-
-    } while (FindNextFile(path, &SPath));
-
-    //check correct path Wav
-
-    FindClose(path);
-
-    /////////////**************************************\\\\\\\mp3
-
-    SystemLog( tier2, "CSLoader.h", "64", "load mp3");
-    path = FindFirstFile(L"../sound/*.mp3*", &SPath);
-    do {
-
-        if (path == INVALID_HANDLE_VALUE) { //no treck
-            ErrorLog( tier2, "CSLoader.h", "67", "FindFirstFile failed", 999 );
-            break;
-        }
-
-
-        //func( CSPath[index], resoult);
-        func( L"../sound/", resoult);
-        std::wstring str = resoult;
-
-        func(SPath.cFileName, resoult);
-        str += resoult;
-
-        Playlist.playlist.push_back(str);
-
-
-    } while (FindNextFile(path, &SPath));
-
-    //check correct path mp3
-
-    FindClose(path);
+	std::filesystem::path path("..\\sound");
+    std::filesystem::recursive_directory_iterator rec(path);
+    std::filesystem::directory_entry wav(".wav");
+	std::filesystem::directory_entry mp3(".mp3");
+	
+    for (auto p : rec) {
+        
+        std::filesystem::path tmp(p);
+		if((tmp.extension() == wav) || (tmp.extension() == mp3))
+			Playlist.playlist.push_back(tmp.wstring());
+		
+    }
     
     if( !Playlist.playlist.size() ) {
-        ErrorLog( tier2, "CSLoader.h", "87", "not element for playing", 999 );
+        ErrorLog( tier2, "CSLoader.h", "40", "not element for playing", 999 );
         newLog.fileWrite();
     }
     
